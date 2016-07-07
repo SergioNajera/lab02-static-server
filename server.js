ff1a456b2be3d4f93d0c4eb6ffa99533a0252d4a
Mauro <jmaurolp@hotmail.com>
2016-07-07 08:59:44 -0500

    lab03 IP-PORT server ip  y puertos tomados del entorno

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 //http
 var http = require("http");
+//obtener informacion del entorno de ejecucion con respecto al IP y al puerto que debemos usar
+var PORT = process.env.PORT || 3000;
+var IP = process.env.IP || '127.0.0.1';
+if(IP=='127.0.0.1'){
+    console.log(">---EJECUTANDO EN MODO LOCAL");
+}else{
+    console.log(">---EJECUTANDO DESDE EL SERVER");
+}
 //crear un servidor basico
 var server = http.createServer(function(req, res){
    //armar la respuesta http
@@ -9,11 +17,11 @@ var server = http.createServer(function(req, res){
        "Server" : "ITGAM@4.2.4"
    });
    //enviamos la respuesta
-   res.write("hola mundo");
+   res.write("hola mundo Najera");
    //cerrar la conexion
    res.end();
 });
 //poer a trabajar al server
-server.listen(3000, '127.0.0.1', function(){
-console.log(">Server Listening @http://localhost:3000 ...");
+server.listen(PORT, IP, function(){
+console.log(`>Server Listening @http://${IP}: ${PORT} ...`);
 });
